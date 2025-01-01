f7ac8394b8612f537d879e27879312240f085faf
Dave Bush <dave@davembush.com>
2025-01-01 14:15:31 -0500

    lint fix

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,8 +1,8 @@
 import type { Linter } from '@typescript-eslint/utils/ts-eslint';
 
 import noAnonymousFunctions from './lib/no-anonymous-functions/no-anonymous-functions';
-import oneExportedItemPerFile from './lib/one-exported-item-per-file/one-exported-item-per-file';
 import noObjectLiterals from './lib/no-object-literals/no-object-literals';
+import oneExportedItemPerFile from './lib/one-exported-item-per-file/one-exported-item-per-file';
 
 // eslint-disable-next-line typescriptEslintPlugin/no-require-imports, typescriptEslintPlugin/no-var-requires -- needed to get information from package.json
 const { name, version } = require('../package.json') as { name: string; version: string };
