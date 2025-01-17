4a5ef8b69956c61b1ada320ffe6c3df36d15312a
Dave Bush <dave@davembush.com>
2025-01-17 10:26:31 -0500

    fix: adds index.d.ts and support esm projects (#23)
    * now supports typing and use from esm files
    * fix lint issues

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,21 +1,35 @@
-import type { Linter } from '@typescript-eslint/utils/ts-eslint';
+import { Linter } from '@typescript-eslint/utils/ts-eslint';
 
 import noAnonymousFunctions from './lib/no-anonymous-functions/no-anonymous-functions';
 import noObjectLiterals from './lib/no-object-literals/no-object-literals';
 import oneExportedItemPerFile from './lib/one-exported-item-per-file/one-exported-item-per-file';
 
 // eslint-disable-next-line typescriptEslintPlugin/no-require-imports, typescriptEslintPlugin/no-var-requires -- needed to get information from package.json
-const { name, version } = require('../package.json') as { name: string; version: string };
+const { name, version } = require('../package.json') as {
+  name: string;
+  version: string;
+};
 
 const meta = {
-	name,
-	version,
+  name,
+  version,
 } satisfies Linter.PluginMeta;
 
 const rules = {
-	'one-exported-item-per-file': oneExportedItemPerFile,
-	'no-anonymous-functions': noAnonymousFunctions,
-	'no-object-literals': noObjectLiterals,
+  'one-exported-item-per-file': oneExportedItemPerFile,
+  'no-anonymous-functions': noAnonymousFunctions,
+  'no-object-literals': noObjectLiterals,
 } satisfies Linter.PluginRules;
 
-module.exports = { meta, rules } satisfies Linter.Plugin;
+interface ESLintPlugin extends Linter.Plugin {
+  meta: typeof meta;
+  rules: typeof rules;
+}
+
+// eslint-disable-next-line @typescript-eslint/naming-convention -- ESLint rule names use kebab-case
+const plugin: ESLintPlugin = {
+  meta,
+  rules,
+};
+
+export = plugin;
